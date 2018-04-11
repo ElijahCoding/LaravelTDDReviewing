3f458b614c0d52a1eb329eb1e7d985aac97b93f2
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-11 19:33:13 +0300

    creat a thread

---
+++ tests/Feature/CreateThreadTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class CreateThreadTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected $thread;
+
+    public function setUp()
+    {
+      parent::setUp();
+      $this->thread = create('App\Thread');
+    }
+
+    /** @test */
+    public function guests_may_not_create_threads()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $this->post('/threads', $this->thread->toArray());
+    }
+
+    /** @test */
+    public function an_authenticated_user_can_create_new_forum_threads()
+    {
+        $this->signIn();
+
+        $this->post('/threads', $this->thread->toArray());
+
+        $this->get($this->thread->path())
+             ->assertSee($this->thread->title)
+             ->assertSee($this->thread->body);
+    }
+}
