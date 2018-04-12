353f015c83899ba3e238bcb93fefba5b66137786
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-12 13:20:19 +0300

    validation testing

--- tests/Feature/CreateThreadTest.php
+++ tests/Feature/CreateThreadTest.php
@@ -24,7 +24,7 @@ class CreateThreadTest extends TestCase
 
         $this->get('/threads/create')
              ->assertRedirect('/login');
-             
+
         $this->post('/threads')
              ->assertRedirect('/login');
     }
@@ -34,10 +34,43 @@ class CreateThreadTest extends TestCase
     {
         $this->signIn();
 
-        $this->post('/threads', $this->thread->toArray());
+        $response = $this->post('/threads', $this->thread->toArray());
 
-        $this->get($this->thread->path())
+        $this->get($response->headers->get('Location'))
              ->assertSee($this->thread->title)
              ->assertSee($this->thread->body);
+    }
+
+    /** @test */
+    public function a_thread_requires_a_title()
+    {
+      $this->publishThread(['title' => null])
+           ->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_thread_requires_a_body()
+    {
+        $this->publishThread(['body' => null])
+            ->assertSessionHasErrors('body');
+    }
+
+    /** @test */
+    public function a_thread_requires_a_valid_channel()
+    {
+      factory('App\Channel', 2)->create();
+      
+      $this->publishThread(['channel_id' => null])
+          ->assertSessionHasErrors('channel_id');
+
+      $this->publishThread(['channel_id' => 999])
+          ->assertSessionHasErrors('channel_id');
+    }
+
+    protected function publishThread($overrides = [])
+    {
+        $this->withExceptionHandling()->signIn();
+        $thread = make('App\Thread', $overrides);
+        return $this->post('/threads', $thread->toArray());
     }
 }
