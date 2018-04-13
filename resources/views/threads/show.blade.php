fc7bf721b5a6a931b361c85d9eb6185a73811ac7
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-13 17:38:19 +0300

    adding global scope

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-8">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <a href="#">{{ $thread->creator->name }}</a> posted:
@@ -14,36 +14,37 @@
                         {{ $thread->body }}
                     </div>
                 </div>
-            </div>
-        </div>
 
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
                 @foreach ($thread->replies as $reply)
                     @include ('threads.reply')
                 @endforeach
+
+                @if (auth()->check())
+
+                      <form action="{{ $thread->path() . '/replies' }}" method="post">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                          <label for="body">Body:</label>
+                          <textarea class="form-control" id="body" name="body" rows="5"></textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-default">Post</button>
+                      </form>
+                @else
+                  <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+                @endif
+            </div>
+
+            <div class="col-md-4">
+              <div class="panel panel-default">
+                  <div class="panel-body">
+                    This thread was published {{ $thread->created_at->diffForHumans() }} by
+                    <a href="#">{{ $thread->creator->name }}</a>, and currently
+                    has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.
+                  </div>
+              </div>
             </div>
         </div>
-
-        @if (auth()->check())
-          <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-              <form action="{{ $thread->path() . '/replies' }}" method="post">
-                {{ csrf_field() }}
-
-                <div class="form-group">
-                  <label for="body">Body:</label>
-                  <textarea class="form-control" id="body" name="body" rows="5"></textarea>
-                </div>
-
-                <button type="submit" class="btn btn-default">Post</button>
-              </form>
-            </div>
-          </div>
-        @else
-          <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
-        @endif
     </div>
-
-
 @endsection
