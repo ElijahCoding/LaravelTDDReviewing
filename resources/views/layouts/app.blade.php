73316e61a68521287c86a1dc9ded1b7818fe8eb2
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-16 13:17:36 +0300

    filtering by popularity which based on replies_count

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,18 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <script>
+      window.laravel = {!! json_encode([
+        'csrfToken' => csrf_token(),
+      ]) !!};
+    </script>
+
+    <style>
+        body { padding-bottom: 100px; }
+        .level { display: flex; align-items: center; }
+        .flex { flex: 1; }
+    </style>
 </head>
 <body style="padding-bottom: 200px">
     <div id="app">
