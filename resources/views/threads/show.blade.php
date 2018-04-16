c2b79a6a6e1744afa70300eecd8dd96604ff42e8
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-16 18:35:10 +0300

    file structure refactoring

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -6,8 +6,21 @@
             <div class="col-md-8">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted:
-                        {{ $thread->title }}
+                        <div class="level">
+                            <span class="flex">
+                                <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted:
+                                {{ $thread->title }}
+                            </span>
+
+                            @if (Auth::check())
+                                <form action="{{ $thread->path() }}" method="POST">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+
+                                    <button type="submit" class="btn btn-link">Delete Thread</button>
+                                </form>
+                            @endif
+                        </div>
                     </div>
 
                     <div class="panel-body">
@@ -22,30 +35,32 @@
                 {{ $replies->links() }}
 
                 @if (auth()->check())
-
-                      <form action="{{ $thread->path() . '/replies' }}" method="post">
+                    <form method="POST" action="{{ $thread->path() . '/replies' }}">
                         {{ csrf_field() }}
 
                         <div class="form-group">
-                          <label for="body">Body:</label>
-                          <textarea class="form-control" id="body" name="body" rows="5"></textarea>
+                            <textarea name="body" id="body" class="form-control" placeholder="Have something to say?"
+                                      rows="5"></textarea>
                         </div>
 
                         <button type="submit" class="btn btn-default">Post</button>
-                      </form>
+                    </form>
                 @else
-                  <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+                    <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this
+                        discussion.</p>
                 @endif
             </div>
 
             <div class="col-md-4">
-              <div class="panel panel-default">
-                  <div class="panel-body">
-                    This thread was published {{ $thread->created_at->diffForHumans() }} by
-                    <a href="#">{{ $thread->creator->name }}</a>, and currently
-                    has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.
-                  </div>
-              </div>
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <p>
+                            This thread was published {{ $thread->created_at->diffForHumans() }} by
+                            <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a>, and currently
+                            has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
