71869081999153873c1b5a6196d2e1da39538f4a
LittleElijah <cuiyeqing960904@gmail.com>
2018-04-18 15:29:35 +0300

    flash-message vue component

---
+++ resources/assets/js/components/Flash.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="alert alert-success alert-flash" role="alert" v-show="show">
+      <strong>Success!</strong> {{ body }}
+</div>
+</template>
+
+<script>
+    export default {
+        props: ['message'],
+
+        data() {
+          return {
+            body: this.message,
+            show: false
+          }
+        },
+
+        created() {
+          if (this.message) {
+            this.body = this.message
+            this.show = true
+          }
+        },
+
+        methods: {
+
+        }
+    }
+</script>
+
+<style>
+    .alert-flash {
+        position: fixed;
+        right: 25px;
+        bottom: 25px;
+    }
+</style>
